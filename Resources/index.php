2771f1b48286c15609b4cd221c69ee47cf378f5e
Santiago Nunez <113261292+Santiago2607@users.noreply.github.com>
2022-10-20 00:36:01 -0400

    Delete index.php

--- Resources/index.php
+++
@@ -1,4 +0,0 @@
-
-![VBA_Challenge_2017 png](https://user-images.githubusercontent.com/113261292/196856818-c27a2120-7525-4c36-9a15-d325d84652e0.png)
-
-![VBA_Challenge_2018 png](https://user-images.githubusercontent.com/113261292/196856843-600fbe53-7f4a-49e3-8fd3-49bec3dc7982.png)
