7a9603afdde7b1f93a492194b937484ced342357
Santiago Nunez <113261292+Santiago2607@users.noreply.github.com>
2022-10-20 00:33:58 -0400

    Update index.php

--- Resources/index.php
+++ Resources/index.php
@@ -1,3 +1,4 @@
-![VBA_Challenge_2017.png.png](VBA_Challenge_2017.png.png)
 
-![VBA_Challenge_2018.png.png](VBA_Challenge_2018.png.png)
+![VBA_Challenge_2017 png](https://user-images.githubusercontent.com/113261292/196856818-c27a2120-7525-4c36-9a15-d325d84652e0.png)
+
+![VBA_Challenge_2018 png](https://user-images.githubusercontent.com/113261292/196856843-600fbe53-7f4a-49e3-8fd3-49bec3dc7982.png)
